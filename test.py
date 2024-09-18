4357fa0fbdf3381cb2d9e843c084ae05f30791bc
poyiray <85799454+poyiray@users.noreply.github.com>
2024-09-18 09:36:26 -0400

    hh

--- test.py
+++ test.py
@@ -1,1 +1,4 @@
-print("123")
+first = input()
+last = input()
+birth = int(input())
+print(f"Hello {first} {last}.\nYou are {2024 - birth} years old.\nYou are {('old enough' if 2024 - birth >= 18 else 'not old enough')} to drink")
